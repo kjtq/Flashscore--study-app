d794b6b3ea6a8ed8b949df09450e34ece469ce7f
MAGAJIco <97481521+MAGAJIco@users.noreply.github.com>
2025-10-01 00:45:40 +0100

    Create App.jsx

---
+++ apps/frontend/src/app/components/App.jsx
@@ -0,0 +1,12 @@
+import ApiTest from "./components/ApiTest";
+
+function App() {
+  return (
+    <div>
+      <h1>MagajiCo Backend Test</h1>
+      <ApiTest />
+    </div>
+  );
+}
+
+export default App;
