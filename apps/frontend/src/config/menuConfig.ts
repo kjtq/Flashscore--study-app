832dac56f26a8fc3c4e66b9638f83cdb8682a704
MAGAJIco <97481521+MAGAJIco@users.noreply.github.com>
2025-10-03 15:56:03 +0100

    Create menuConfig.ts

---
+++ apps/frontend/src/config/menuConfig.ts
@@ -0,0 +1,8 @@
+// apps/frontend/src/config/menuConfig.ts
+export const menuItems = [
+  { label: "Home", href: "/" },
+  { label: "News", href: "/news" },
+  { label: "Predictions", href: "/predictions" },
+  { label: "Authors", href: "/authors" },
+  { label: "Management", href: "/management" },
+];
